2cff719610306f3608de1df33f75ca5439fa6399
NvdYmn <navidyamini@gmail.com>
2018-03-02 13:25:42 +0100

    Add files via upload

---
+++ ICT in Transport System/lab1_2_1_on24h/Output Data/percentage.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 31 13:31:14 2018
+
+@author: Matteo
+"""
+import matplotlib
+from matplotlib import pyplot
+import os
+import numpy as np
+
+
+
+if __name__=='__main__':
+    bookings_d = {}
+    parkings_d = {}
+    cities = ['MA','TO','NY']
+    
+    for city in cities:
+        folder = os.path.dirname(os.path.abspath(__file__))
+        #%% Bookings filtered
+        bookings = folder + "/booking_"+city+".dat"    
+        f=open(bookings,"r")
+        booking_lines=f.readlines()
+        bookings_d[city] = {}
+        previous = -1
+        y = []
+        for l in booking_lines:
+            key = int(l.split(' ')[1])
+            value = int(l.split(' ')[2])        
+    
+            while (int(key) - previous) != 1:
+                bookings_d[city][previous+1] = 0
+                previous += 1
+                
+            bookings_d[city][key] = value
+            previous = int(key)
+            
+                    
+        
+        f.close()       
+
+        
+        #%% Parkings filtered
+        parkings = folder + "/parking_"+city+".dat"    
+        f=open(parkings,"r")
+        parking_lines=f.readlines()
+        x=[]
+        y=[]
+        previous = -1
+        parkings_d[city] = {}
+        
+        for l in parking_lines:
+            key = int(l.split(' ')[1])
+            value = int(l.split(' ')[2])   
+            while (int(key) - previous) != 1:
+                parkings_d[city][previous+1] = 0
+                previous += 1
+                
+            parkings_d[city][key] = value
+            previous = key
+            
+        f.close()
+    
+#%% computing percentage
+    percentage_b = {}
+    percentage_p = {}
+    total = {}
+    fig = pyplot.figure(3, figsize=(20,10))
+    matplotlib.rcParams.update({'font.size': 35})
+
+    pyplot.title('Percentage of Bookings per Hour of the Day in September 2017')
+    pyplot.xlabel('Hour of The Day')
+    pyplot.ylabel('Utilization Factor')
+    for city in cities:
+        percentage_b[city] = {}
+        percentage_p[city] = {}
+        total[city] = {}
+        for hour in bookings_d[city]:
+            try:
+                total[city][hour] = bookings_d[city][hour] + parkings_d[city][hour]
+            except KeyError as e:
+                print ('Missing Values')
+                total[city][hour] = -1
+                           
+            
+            try:
+                percentage_b[city][hour] = float(bookings_d[city][hour])/total[city][hour]
+            except ZeroDivisionError as zerodiv:
+                percentage_b[city][hour] = 0
+        x = np.linspace(0, 23, 24)
+        pyplot.plot(x, list(percentage_b[city].values()), label=city,linewidth=3.5)
+        pyplot.legend()
+        fig.savefig(folder + '/percentage.png')
+    
+      
+        
+        
+    
+        
---
+++ ICT in Transport System/lab1_2_1_on24h/lab1_2_1_parking_nofilter.js
@@ -0,0 +1,66 @@
+//System utilization over time: aggregate rentals per hour of the day,
+var startDate = new ISODate("2017-09-01T00:00:00")
+var startUnixTime = startDate.getTime() /1000
+var endDate = new ISODate("2017-09-30T23:59:59")
+var endUnixTime = endDate.getTime() / 1000
+var cities = ["Torino","Madrid","New York City"]
+
+var time=[-2,-2,4]
+
+
+var range = 3600
+
+for (i=0; i<cities.length; i++){
+	c = cities[i]
+	w = time[i]
+	add_time = w*60*60
+	var result = db.PermanentBookings.aggregate([
+    {
+        $match: { // filter here what you want first
+            city: c,
+			init_time: { $gte: startUnixTime+add_time, $lte: endUnixTime+add_time}
+            }
+    },
+        {
+        $project: { // extract position, time, duration
+            _id: 0,
+            city: 1,
+            duration: { $divide: [ { $subtract: ["$final_time", "$init_time"] }, 60 ] },
+            hourDay: {$hour: "$init_date"},
+            monthDay: {$concat: [{$substr: [{$month: "$init_date"},0,2]},"-",{$substr: [{$dayOfMonth: "$init_date"},0,2]}]},
+            dayU: {$floor: {$divide: ["$init_time", range]}},
+            init_time: 1,
+            monthDay: {$month: "$init_date"},
+            day: {$dayOfMonth: "$init_date"},
+            init_date:1
+        }
+        
+     },
+{
+    $group:
+    {
+        _id:{hourDay: "$hourDay", city:"$city"},
+        total_parking: {$sum: 1}
+    }
+},
+{
+   $sort:{
+       "_id.city":1,
+       "_id.hourDay":1
+        }
+    }
+ ])
+        
+while(result.hasNext()) {
+    a = result.next()
+	if (a["_id"]["city"] == "New York City")
+	{
+		    print ("NYC", a["_id"]["hourDay"], a["total_parking"])
+	}
+		
+	else 
+    {
+		print (a["_id"]["city"],a["_id"]["hourDay"],a["total_parking"])
+	}
+}
+}
